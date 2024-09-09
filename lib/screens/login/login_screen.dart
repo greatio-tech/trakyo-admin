ccfa81cff6f4738c882ee2f0614e3d243a55cc80
shameerpc5029 <shameerpc5029@gmail.com>
2024-09-09 11:27:06 +0530

    login ui fix

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -19,61 +19,65 @@ class LoginScreen extends StatelessWidget {
           alignment: Alignment.bottomCenter,
           children: [
             Center(
-              child: Padding(
-                padding: EdgeInsets.only(bottom: 200.h),
-                child: Container(
-                  padding: const EdgeInsets.all(24),
-                  decoration: BoxDecoration(
-                    color: AppColors.backgroundColor,
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  width: 320.w,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      const SvgIcon(icon: 'assets/svg/trakyo logo.svg'),
-                      const VSpace(32),
-                      TextWidget(
-                        text: 'Login to your account',
-                        fontSize: 24.sp,
-                        fontWeight: FontWeight.w500,
-                      ),
-                      const VSpace(5),
-                      TextWidget(
-                        text: 'Welcome back!',
-                        fontSize: 14.sp,
-                        textColor: AppColors.textGreyColor,
-                      ),
-                      const VSpace(24),
-                      const TextFiledWidget(
-                        headingText: 'Mail',
-                        hintText: 'Enter your Mail',
-                      ),
-                      const VSpace(16),
-                      const TextFiledWidget(
-                        headingText: 'Password',
-                        hintText: 'Enter your password',
-                      ),
-                      const VSpace(32),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 40),
-                        child: ButtonWidget(
-                          text: 'Login',
-                          color: AppColors.primaryColor,
-                          textColor: AppColors.textWhiteColor,
-                          onTap: () {
-                            Get.offAllNamed('/dashboard');
-                          },
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(24),
+                    decoration: BoxDecoration(
+                      color: AppColors.backgroundColor,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    width: 320.w,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        const SvgIcon(icon: 'assets/svg/trakyo logo.svg'),
+                        const VSpace(32),
+                        TextWidget(
+                          text: 'Login to your account',
+                          fontSize: 24.sp,
+                          fontWeight: FontWeight.w500,
                         ),
-                      ),
-                    ],
+                        const VSpace(5),
+                        TextWidget(
+                          text: 'Welcome back!',
+                          fontSize: 14.sp,
+                          textColor: AppColors.textGreyColor,
+                        ),
+                        const VSpace(24),
+                        const TextFiledWidget(
+                          headingText: 'Mail',
+                          hintText: 'Enter your Mail',
+                        ),
+                        const VSpace(16),
+                        const TextFiledWidget(
+                          headingText: 'Password',
+                          hintText: 'Enter your password',
+                        ),
+                        const VSpace(32),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 40),
+                          child: ButtonWidget(
+                            text: 'Login',
+                            color: AppColors.primaryColor,
+                            textColor: AppColors.textWhiteColor,
+                            onTap: () {
+                              Get.offAllNamed('/dashboard');
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
+                  const VSpace(220),
+                ],
               ),
             ),
-            const SvgIcon(
+            SvgIcon(
+              width: 1500.w,
               icon: 'assets/svg/login image.svg',
             )
           ],
